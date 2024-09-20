b85fbd41264b2d6d9b4ab6e3d35ed0e4ae8124bb
htrann204 <nguyenthihuyentranviettel@gmail.com>
2024-09-20 03:08:35 +0000

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function addToCart(flowerName, price) {
-    alert(flowerName + " has been added to your cart at $" + price);
-}
+// Thêm mã JavaScript nếu cần thiết
+$(document).ready(function() {
+    console.log("Website đã sẵn sàng!");
+    // Thêm các chức năng JavaScript khác nếu cần
+});
